d0092219186152a4d372b3217cb7c4050a7f18e4
Karolina <karola.rzucidlo14@gmail.com>
2025-02-07 22:20:56 +0100

    feat: add new component Task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { USERS } from '../users';
 import { HeaderComponent } from './components/header/header.component';
+import { TaskComponent } from './components/task/task.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgFor, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
