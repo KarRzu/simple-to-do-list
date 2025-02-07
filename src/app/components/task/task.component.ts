cb8f8116c2414e9fe78bcb45d30eebda9b383124
Karolina <karola.rzucidlo14@gmail.com>
2025-02-07 23:52:49 +0100

    feat: generate Todo service

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.scss'
+  styleUrl: './task.component.scss',
 })
 export class TaskComponent {
+  taskForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    description: new FormControl('', Validators.required),
+    priority: new FormControl('High', Validators.required),
+    storyPoints: new FormControl('1', Validators.required),
+  });
 
+  onSubmit() {
+    console.log(this.taskForm.value);
+  }
 }
